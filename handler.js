d20a9a6fe7076334a81c7b0690735d47b6783cbd
Anthony Kim <fbssaejun@gmail.com>
2022-05-09 14:06:20 -0400

    create new endpoint, new lambda function for the new endpoint

--- handler.js
+++ handler.js
@@ -5,8 +5,23 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: 'This is my first Serverless application!',
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.imageRezise = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Resized your image!',
       },
       null,
       2
