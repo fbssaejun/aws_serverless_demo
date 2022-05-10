c01fa739c17fd2825fbf076584ae30c72995b2c5
Anthony Kim <fbssaejun@gmail.com>
2022-05-09 22:04:05 -0400

    create new handler function for author endpoint

--- handler.js
+++ handler.js
@@ -31,3 +31,19 @@ module.exports.imageResize = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.author = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Author: Anthony Kim',
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
